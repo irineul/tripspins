8f8eaa4c8e23074fe7fe141683e5931203ee2426
Irineu Licks Filho <irineul@MacBook-Pro-de-Irineu.local>
2014-02-22 19:44:06 -0300

    Pin Detail and opening the picture. New interface for New Trip

---
+++ OnTheRoadV2/_PinImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  _PinImageViewController.h
+//  OnTheRoadV2
+//
+//  Created by Irineu Licks Filho on 22/02/14.
+//  Copyright (c) 2014 On The Road. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _PinImageViewController : UIViewController
+
+@end
---
+++ OnTheRoadV2/_PinImageViewController.m
@@ -0,0 +1,38 @@
+//
+//  _PinImageViewController.m
+//  OnTheRoadV2
+//
+//  Created by Irineu Licks Filho on 22/02/14.
+//  Copyright (c) 2014 On The Road. All rights reserved.
+//
+
+#import "_PinImageViewController.h"
+
+@interface _PinImageViewController ()
+
+@end
+
+@implementation _PinImageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
